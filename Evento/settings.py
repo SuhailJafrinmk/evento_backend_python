dcb3fd479c0ebf9061ffff925a559bc92de9ed2d
suhailjafrin <suhailmk196@gmail.com>
2024-07-19 09:38:45 +0530

    changed allowed hosts

--- Evento/settings.py
+++ Evento/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "localhost,127.0.0.1,[::1]").split(",")
+ALLOWED_HOSTS = ["*"]
 
 
 
